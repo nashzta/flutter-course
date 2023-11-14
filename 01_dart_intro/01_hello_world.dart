171303bdc6f30e005667a2f4294eb8fc5744fe68
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2023-11-13 22:09:46 -0500

    first commit - what is dart?

---
+++ 01_dart_intro/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  
+  //var myName = "Jean Pierre";  
+  //late final myName; 
+  String myName = "Jean Pierre";
+  
+  print("Hola $myName");
+  print("Hola ${myName.toUpperCase()}");
+  print("Hola ${ 1 + 1}");
+}
